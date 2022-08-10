186253f11b08a266649a8db942c5e684c13d4968
Aqib <aqibr777@gmail.com>
2022-08-10 16:52:34 +0500

    10 Auto-loading Model

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -3,7 +3,24 @@
 class User_model extends CI_Model {
 
     public function get_users() {
-        
+
+        $query = $this->db->get('users');
+        return $query->result();
+
+        // $config['hostname'] = "localhost";
+        // $config['username'] = "root";
+        // $config['password'] = "";
+        // $config['database'] = "codeiginiter_ci_errand-db";
+
+        // $config_2['hostname'] = "localhost";
+        // $config_2['username'] = "root_2";
+        // $config_2['password'] = "";
+        // $config_2['database'] = "codeiginiter_ci_errand-db_2";
+
+        // $connection = $this->load->database($config);
+
+        // $connection_2 = $this->load->database($config_2);
+
     }
 }
 
