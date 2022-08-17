cbf02826a903c7663dc8df5443a3b8430a6d93eb
Aqib <aqibr777@gmail.com>
2022-08-17 16:59:30 +0500

    4.Conditions-in-our-queries

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -5,7 +5,7 @@ class Users extends CI_Controller {
 
     public function show($user_id) {
 
-        $data['results'] = $this->user_model->get_users($user_id);
+        $data['results'] = $this->user_model->get_users($user_id, 'user 1');
 
         $this->load->view('user_view', $data);
         
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -2,9 +2,13 @@
 
 class User_model extends CI_Model {
 
-    public function get_users($user_id) {
+    public function get_users($user_id, $username) {
 
-        $this->db->where('id', $user_id);
+        $this->db->where([
+            'id' => $user_id,
+            'username' => $username
+        ]);
+
         $query = $this->db->get('users');
 
         return $query->result();
